bffba1d341963cc8d8e2fb0e249ca46e676481b8
catherinewang@TECH0185 <catherine.wang@bounteous.com>
2021-08-23 17:04:26 -0400

    Create-Sample-Users-In-MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 // creste app from express
 const app = express();
-// define route
-app.get('/',(req, res)=> {
-    // response
-    res.send("Server is ready");
-});
 
-app.get('/api/products',(req, res)=>{
-    res.send(data.products)
-})
+mongoose.connect(process.env.MONGODY_URL || 'mongodb://localhost/easy-shop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 app.get('/api/products/:id',(req, res)=>{
     const product = data.products.find((product) => product._id === req.params.id) // 要写在一行
@@ -21,6 +20,22 @@ app.get('/api/products/:id',(req, res)=>{
         res.status(404).send({message:'product not found'})
     }
 })
+app.get('/api/products',(req, res)=>{
+    res.send(data.products)
+})
+
+// use user router
+app.use('/api/users', userRouter);
+
+// define route
+app.get('/',(req, res)=> {
+    // response
+    res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+  });
 
 const port = process.env.PORT || 5000;
 // call listen method of app
